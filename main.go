f760f17fa55512ddd1d9d027e291f5ad4badb7a9
Tomoki <yoshioka.0315527@gmail.com>
2025-06-07 01:52:07 +0900

    greetパッケージを追加し、HelloおよびGoodnight関数を実装

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/Tomhack24/go_practice/greet" // Importing the greet package
+)
+
+func main() {
+	greet.Hello()             // Calling the Hello function from the greet package
+	greet.HelloJapanese()     // Calling the HelloJapanese function from the greet package
+	greet.Goodnight()         // Calling the Goodnight function from the greet package
+	greet.GoodnightJapanese() // Calling the GoodnightJapanese function from the greet package
+}
